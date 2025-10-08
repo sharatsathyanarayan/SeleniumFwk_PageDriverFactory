11ed73496618828fc437fbb1da00ed6980664b9a
sharatsathyanarayan <128732630+sharatsathyanarayan@users.noreply.github.com>
2025-10-07 23:41:06 -0700

    running fireforx and chrome 6 tests in parallel

--- pagedriverfactoryfwk/src/main/java/frameworktwo/base/classes/BaseTest.java
+++ pagedriverfactoryfwk/src/main/java/frameworktwo/base/classes/BaseTest.java
@@ -9,38 +9,45 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class BaseTest {
 
 	WebDriver driver = null;
 	protected WebDriverWait wait = null;
-
+	protected String browser = null;
+	
+	@Parameters({"browsername"})
 	@BeforeClass
-	public void setUp() {
-		
-		//setting up for local
-		
-		 // Turn off password manager, autofill & leak detection
-		// Create a HashMap to store Chrome preferences
-        Map<String, Object> chromePrefs = new HashMap<>();
-        // Disable the credential enable service (prevents saving passwords)
-        chromePrefs.put("credentials_enable_service", false);
-        // Disable the built-in password manager
-        chromePrefs.put("profile.password_manager_enabled", false);
-        // Disable password leak detection (prevents the breach notification)
-        chromePrefs.put("profile.password_manager_leak_detection", false);
-        // Create ChromeOptions and set the experimental preferences
-        ChromeOptions options = new ChromeOptions();
-        options.setExperimentalOption("prefs", chromePrefs);
+	public void setUp (String browsername) {
 
+		// setting up for local
+
+		Map<String, Object> chromePrefs = new HashMap<>();
+		chromePrefs.put("credentials_enable_service", false);
+		chromePrefs.put("profile.password_manager_enabled", false);
+		chromePrefs.put("profile.password_manager_leak_detection", false);
+		ChromeOptions options = new ChromeOptions();
+		options.setExperimentalOption("prefs", chromePrefs);
+		this.browser = browsername;
 		
-		if(Config.env.equals("local") && Config.browser.equals("chrome")) {
-		driver = new ChromeDriver(options);
-		PageDriver.getInstance().setDriver(driver);
-		driver.get(Config.url);
-		driver.manage().window().maximize();
-		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(5));
+		//if (Config.env.equals("local") && Config.browser.equalsIgnoreCase("chrome")) {
+		if (Config.env.equals("local") && this.browser.equalsIgnoreCase("chrome")) {
+			driver = new ChromeDriver(options);
+			PageDriver.getInstance().setDriver(driver);
+			driver.get(Config.url);
+			driver.manage().window().maximize();
+			driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(5));
+		}
+		//if (Config.env.equals("local") && Config.browser.equalsIgnoreCase("firefox")) {
+		if (Config.env.equals("local") && this.browser.equalsIgnoreCase("firefox")) {
+			driver = new FirefoxDriver();
+			PageDriver.getInstance().setDriver(driver);
+			driver.get(Config.url);
+			driver.manage().window().maximize();
+			driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(5));
 		}
 
 	}
--- pagedriverfactoryfwk/src/main/java/frameworktwo/base/classes/Config.java
+++ pagedriverfactoryfwk/src/main/java/frameworktwo/base/classes/Config.java
@@ -5,7 +5,7 @@ public class Config {
 	
 	// local config
 	
-	public static String browser = System.getProperty("browser", "chrome");
+	public static String browser = System.getProperty("browser", "firefox");
 	public static String env = System.getProperty("env", "local");
 	public static String validUserName = System.getProperty("validUserName", "standard_user");
 	public static String ValidPassword = System.getProperty("ValidPassword", "secret_sauce");
